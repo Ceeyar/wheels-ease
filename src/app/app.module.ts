25c7c3316544476fe7c890d3562f9c9d5361d7aa
Siyabonga <cndovela@retrorabbit.co.za>
2018-09-13 11:47:02 +0200

    sidenav and topfixed nav with no styling/

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
-import { MDBBootstrapModule } from 'angular-bootstrap-md';
-
+import { SidebarModule } from 'ng-sidebar';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './/app-routing.module';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { MainComponent } from './components/main/main.component';
+import { NavbarModule, WavesModule } from 'angular-bootstrap-md';
 import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    MainComponent
   ],
   imports: [
+    NavbarModule, WavesModule,
     BrowserModule,
     MDBBootstrapModule.forRoot(),
+    SidebarModule.forRoot(),
     AppRoutingModule
   ],
   schemas: [ NO_ERRORS_SCHEMA ],
